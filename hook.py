55c9698fa30b41faecf41f6e855ceb9204f391a7
Hunt, David <davidhunt@mitre.org>
2019-04-17 15:53:51 -0400

    swapping submodule for plugin

--- hook.py
+++ hook.py
@@ -1,10 +1,10 @@
 name = 'Open'
 description = 'All open-source abilities, adversaries and files'
 address = None
-store = 'submodules/open/filestore'
+store = 'plugins/open/filestore'
 
 
 async def initialize(app, services):
     data_svc = services.get('data_svc')
-    await data_svc.reload_database(adversaries='submodules/open/adversaries.yml',
-                                   abilities='submodules/open/abilities')
+    await data_svc.reload_database(adversaries='plugins/open/adversaries.yml',
+                                   abilities='plugins/open/abilities')
