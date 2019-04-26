66ff4b2c43961971271dc49f71cc89b71b7d7506
Hunt, David <davidhunt@mitre.org>
2019-04-26 12:34:33 -0400

    adding jitter

--- app/sequential.py
+++ app/sequential.py
@@ -6,8 +6,9 @@ from datetime import datetime
 
 class LogicalPlanner:
 
-    def __init__(self, data_svc, log):
+    def __init__(self, data_svc, utility_svc, log):
         self.data_svc = data_svc
+        self.utility_svc = utility_svc
         self.log = log
         self.loop = asyncio.get_event_loop()
 
@@ -19,13 +20,12 @@ class LogicalPlanner:
     async def _exhaust_agent(self, agent, operation, phase):
         while True:
             operation = await self.wait_for_agent(operation['id'], agent['id'])
-            link = await self.choose_next_link(operation, agent, phase)
+            link, cleanup = await self.choose_next_link(operation, agent, phase)
             if not link:
                 break
-            await self.data_svc.dao.create('core_chain', link)
+            await self.data_svc.create_link(link, cleanup)
 
-    @staticmethod
-    async def choose_next_link(operation, agent, phase):
+    async def choose_next_link(self, operation, agent, phase):
         host_already_ran = [l['command'] for l in operation['chain'] if l['host_id'] == agent['id'] and l['collect']]
         phase_abilities = [i for p, v in operation['adversary']['phases'].items() if p <= phase for i in v]
         for a in phase_abilities:
@@ -36,12 +36,14 @@ class LogicalPlanner:
             variables = re.findall(r'#{(.*?)}', decoded_test, flags=re.DOTALL)
             if encoded_test not in host_already_ran and not variables:
                 return dict(op_id=operation['id'], host_id=agent['id'], ability_id=a['id'], decide=datetime.now(),
-                            command=encoded_test, score=0)
+                            command=encoded_test, score=0, jitter=self.utility_svc.jitter(operation['jitter'])), \
+                       dict(op_id=operation['id'], agent_id=agent['id'], command=a.get('cleanup'), ability_id=a['id'])
+        return None, None
 
     async def wait_for_agent(self, op_id, agent_id):
         op = await self.data_svc.explode_operation(dict(id=op_id))
         while await self._uncollected_links(op[0], agent_id):
-            await asyncio.sleep(1)
+            await asyncio.sleep(2)
             op = await self.data_svc.explode_operation(dict(id=op_id))
         return op[0]
 
