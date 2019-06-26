c30670543d76a2b5e8da9c5ee6f8dc9c0a27dd87
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-06-25 20:02:15 -0400

    ensuring facts can be used from the UI (#46)
    Former-commit-id: fdc7206500e2f255385f50e3c9dd1c5061c7e914

--- app/sequential.py
+++ app/sequential.py
@@ -1,61 +1,12 @@
-import asyncio
-import re
-from base64 import b64decode
-from datetime import datetime
-
-
 class LogicalPlanner:
 
-    def __init__(self, data_svc, utility_svc, log):
+    def __init__(self, data_svc, planning_svc):
         self.data_svc = data_svc
-        self.utility_svc = utility_svc
-        self.log = log
-        self.loop = asyncio.get_event_loop()
+        self.planning_svc = planning_svc
 
     async def execute(self, operation, phase):
         for member in operation['host_group']['agents']:
             agent = await self.data_svc.dao.get('core_agent', dict(id=member['agent_id']))
-            await self._exhaust_agent(agent[0], operation, phase)
-
-    async def _exhaust_agent(self, agent, operation, phase):
-        while True:
-            operation = await self.wait_for_agent(operation['id'], agent['id'])
-            if not await self.choose_next_link(operation, agent, phase):
-                break
-
-    async def choose_next_link(self, operation, agent, phase):
-        completed_tests = [l['command'] for l in operation['chain'] if l['host_id'] == agent['id'] and l['collect']]
-        phase_abilities = [i for p, v in operation['adversary']['phases'].items() if p <= phase for i in v]
-        phase_abilities[:] = [p for p in phase_abilities if agent['platform'] == p['platform']]
-        links = False
-        for a in phase_abilities:
-            decoded_test = b64decode(a['test']).decode('utf-8')
-            decoded_test = decoded_test.replace('#{server}', agent['server'])
-            decoded_test = decoded_test.replace('#{group}', operation['host_group']['name'])
-            decoded_test = decoded_test.replace('#{files}', agent['files'])
-            encoded_test = await self._apply_stealth(operation, agent, decoded_test)
-            variables = re.findall(r'#{(.*?)}', decoded_test, flags=re.DOTALL)
-            if encoded_test not in completed_tests and not variables:
-                links = True
-                await self.data_svc.dao.create('core_chain',
-                                               dict(op_id=operation['id'], host_id=agent['id'], ability=a['id'],
-                                                    decide=datetime.now(), score=0, command=encoded_test,
-                                                    cleanup=a.get('cleanup') if operation['cleanup'] else '',
-                                                    jitter=self.utility_svc.jitter(operation['jitter'])))
-        return links
-
-    async def wait_for_agent(self, op_id, agent_id):
-        op = await self.data_svc.explode_operation(dict(id=op_id))
-        while await self._uncollected_links(op[0], agent_id):
-            await asyncio.sleep(2)
-            op = await self.data_svc.explode_operation(dict(id=op_id))
-        return op[0]
-
-    @staticmethod
-    async def _uncollected_links(operation, agent_id):
-        return next((lnk for lnk in operation['chain'] if lnk['host_id'] == agent_id and not lnk['finish']), False)
-
-    async def _apply_stealth(self, operation, agent, decoded_test):
-        if operation['stealth']:
-            decoded_test = self.utility_svc.apply_stealth(agent['platform'], decoded_test)
-        return self.utility_svc.encode_string(decoded_test)
+            for l in await self.planning_svc.select_links(operation, agent[0], phase):
+                await self.data_svc.dao.create('core_chain', l)
+            await self.planning_svc.wait_for_phase(operation['id'], agent[0]['id'])
