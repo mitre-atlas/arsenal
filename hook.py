8568d5b891e2798d12cfd7059ee6adb1a362990c
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-07-09 18:07:57 -0400

    ensuring planner can be chosen from gui (#57)
    Former-commit-id: decda1750896a26a85ba2e59ba9e6c33471deb48

--- hook.py
+++ hook.py
@@ -7,4 +7,5 @@ async def initialize(app, services):
     data_svc = services.get('data_svc')
     await data_svc.reload_database(adversaries='plugins/stockpile/adversaries.yml',
                                    abilities='plugins/stockpile/abilities',
-                                   facts='plugins/stockpile/facts.yml')
+                                   facts='plugins/stockpile/facts.yml',
+                                   planner=dict(name='sequential', module='plugins.stockpile.app.sequential'))
