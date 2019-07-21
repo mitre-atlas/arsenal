78c8bc09a91b02e72bba2011a38a4f03885daf9f
brianedmonds90 <brianedmonds90@gmail.com>
2019-07-21 07:54:31 -0400

    Virts 258 (#81)
    * parsing mimikatz to add relationship between username and password POC. Also added semicolon after each powershell command so that it works on Powershell 2.0+ (Windows 7)
    
    * creating a separate parsing module
    
    * promoting parsing functions to first class citizens
    
    
    Former-commit-id: 22e6430bb585cf81180745b9d9ba4ce882ed3e1f

---
+++ parsers/mimikatz_parser.py
@@ -0,0 +1,15 @@
+
+def parse_mimikatz(blob):
+    set_id = 0
+    matched_facts = []
+    list_lines = blob.split('\n')
+    for i, line in enumerate(list_lines):
+        if 'Username' in line and '(null)' not in line:
+            username_fact = dict(fact='host.user.name', value=line.split(':')[1].strip(), set_id=set_id)
+            if 'Password' in list_lines[i + 2] and '(null)' not in list_lines[i + 2]:
+                password_fact = dict(fact='host.user.password', value=list_lines[i + 2].split(':')[1].strip(),
+                                     set_id=set_id)
+                matched_facts.append(password_fact)
+                matched_facts.append(username_fact)
+            set_id += 1
+    return matched_facts
---
+++ parsers/standard_parsers.py
@@ -0,0 +1,31 @@
+import json
+import re
+
+def json(parser, blob):
+    matched_facts = []
+    if blob:
+        structured = json.loads(blob)
+        if isinstance(structured, (list,)):
+            for i, entry in enumerate(structured):
+                matched_facts.append((dict(fact=parser['property'], value=entry.get(parser['script']), set_id=i)))
+        elif isinstance(structured, (dict,)):
+            dict_match = parser['script']
+            dict_match = dict_match.split(',')
+            match = structured
+            for d in dict_match:
+                match = match[d]
+            matched_facts.append((dict(fact=parser['property'], value=match, set_id=0)))
+        else:
+            matched_facts.append((dict(fact=parser['property'], value=structured[parser['script']], set_id=0)))
+    return matched_facts
+
+
+def regex(parser, blob):
+    matched_facts = []
+    for i, v in enumerate([m for m in re.findall(parser['script'], blob)]):
+        matched_facts.append(dict(fact=parser['property'], value=v, set_id=i))
+    return matched_facts
+
+def line(parser, blob):
+    return [dict(fact=parser['property'], value=f.strip(), set_id=0) for f in blob.split('\n') if f]
+
