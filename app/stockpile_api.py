c56a9b9ae0e5f6717cc8a721876aa7a751671674
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-07-30 10:37:34 -0400

    ensuring abilities are read from all plugins (#92)
    Former-commit-id: 32be9cda4f76ab1defa52c35b8acf5c84cbdf0a0

--- app/stockpile_api.py
+++ app/stockpile_api.py
@@ -1,3 +1,5 @@
+import os
+
 from aiohttp import web
 from pathlib import Path
 
@@ -10,6 +12,10 @@ class StockpileApi:
     async def load_ability(self, request):
         await self.services.get('auth_svc').check_permissions(request)
         ability = dict(await request.json())
-        for filename in Path('plugins/stockpile/abilities').glob('**/%s.yml' % ability['ability_id']):
-            ability_file = filename.read_text()
-            return web.Response(body=ability_file)
+        ability_dirs = ['plugins/%s/abilities' % p.name.lower() for p in self.services.get('plugins')
+                        if os.path.isdir('plugins/%s/abilities' % p.name.lower())]
+        for d in ability_dirs:
+            for filename in Path(d).glob('**/%s.yml' % ability['ability_id']):
+                ability_file = filename.read_text()
+                return web.Response(body=ability_file)
+        return web.Response(body='No ability file found!')
