70c5244fcf8ce0aae746d157c94ac819ed593866
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-08-01 07:50:44 -0400

    ensuring dao is not used directly (#96)
    Former-commit-id: 6706964f7c71b16da9bc30a06a1bcc0b7d762053

--- app/sequential.py
+++ app/sequential.py
@@ -6,9 +6,8 @@ class LogicalPlanner:
 
     async def execute(self, operation, phase):
         for member in operation['host_group']['agents']:
-            agent = await self.data_svc.dao.get('core_agent', dict(id=member['agent_id']))
+            agent = await self.data_svc.explode_agents(criteria=dict(id=member['agent_id']))
             for l in await self.planning_svc.select_links(operation, agent[0], phase):
                 l.pop('rewards', [])
-                await self.data_svc.dao.create('core_chain', l)
+                await self.data_svc.create_link(l)
         await self.planning_svc.wait_for_phase(operation)
-
