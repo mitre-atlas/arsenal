b4bf49f77e360dff30230b434d675c0086ec2902
hf-mitre <31480194+hf-mitre@users.noreply.github.com>
2019-08-23 12:09:48 -0400

    allow sequential planner execution to be paused (#122)
    Former-commit-id: 5e5dfe82095004d3b011672e12f7c8d518a20e02

--- app/sequential.py
+++ app/sequential.py
@@ -2,11 +2,11 @@ class LogicalPlanner:
 
     def __init__(self, planning_svc):
         self.planning_svc = planning_svc
-        self.data_svc = planning_svc.get_service('data_svc')
+        self.agent_svc = planning_svc.get_service('agent_svc')
 
     async def execute(self, operation, phase):
         for member in operation['host_group']:
             for l in await self.planning_svc.select_links(operation, member, phase):
                 l.pop('rewards', [])
-                await self.data_svc.create_link(l)
+                await self.agent_svc.perform_action(l)
         await self.planning_svc.wait_for_phase(operation)
