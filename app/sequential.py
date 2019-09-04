e20a8b1c36e3a4710fbc4c73a21d257863cf97ce
david <49954156+privateducky@users.noreply.github.com>
2019-09-04 10:16:38 -0400

    changing operation service to have operation object in constructor (#147)
    Former-commit-id: 4ff126b942d334d500d2c9f1aa8e97bc288aaa5a

--- app/sequential.py
+++ app/sequential.py
@@ -1,10 +1,11 @@
 class LogicalPlanner:
 
-    def __init__(self, planning_svc):
+    def __init__(self, operation, planning_svc):
+        self.operation = operation
         self.planning_svc = planning_svc
         self.agent_svc = planning_svc.get_service('agent_svc')
 
-    async def execute(self, operation, phase):
+    async def execute(self, phase):
         for member in operation['host_group']:
             for l in await self.planning_svc.select_links(operation, member, phase):
                 l.pop('rewards', [])
