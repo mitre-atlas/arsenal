9399f954df4d89a6a0341f4cedb9571031cfba81
Caleb Little <50213210+ArtificialErmine@users.noreply.github.com>
2019-09-11 15:03:50 -0400

    Minor tweaks to prevent caldera from picking up machine accounts (#154)
    Former-commit-id: 128dc2431b93e1a0b3655b73218e340b47d28b83

--- parsers/mimikatz.py
+++ parsers/mimikatz.py
@@ -5,11 +5,13 @@ def mimikatz(blob, log):
     list_lines = blob.split('\n')
     for i, line in enumerate(list_lines):
         if 'Username' in line and '(null)' not in line:
-            username_fact = dict(fact='host.user.name', value=line.split(':')[1].strip(), set_id=set_id)
-            if 'Password' in list_lines[i + 2] and '(null)' not in list_lines[i + 2]:
-                password_fact = dict(fact='host.user.password', value=list_lines[i + 2].split(':')[1].strip(),
-                                     set_id=set_id)
-                matched_facts.append(password_fact)
-                matched_facts.append(username_fact)
-            set_id += 1
+            value = line.split(':')[1].strip()
+            if value[-1] is not '$':
+                username_fact = dict(fact='host.user.name', value=value, set_id=set_id)
+                if 'Password' in list_lines[i + 2] and '(null)' not in list_lines[i + 2]:
+                    password_fact = dict(fact='host.user.password', value=list_lines[i + 2].split(':')[1].strip(),
+                                         set_id=set_id)
+                    matched_facts.append(password_fact)
+                    matched_facts.append(username_fact)
+                    set_id += 1
     return matched_facts
