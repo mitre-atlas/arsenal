302c85474c2e6d3133d415e49e41259583f94188
david <49954156+privateducky@users.noreply.github.com>
2019-10-23 12:50:05 -0400

    reworking data_svc to control the flow (#177)
    Former-commit-id: dc47a11b8cc12a34caba90ce2b63ef9a16ce2f4e

--- app/sequential.py
+++ app/sequential.py
@@ -7,7 +7,7 @@ class LogicalPlanner:
         self.data_svc = planning_svc.get_service('data_svc')
 
     async def execute(self, phase):
-        operation = (await self.data_svc.explode_operation(dict(id=self.operation['id'])))[0]
+        operation = (await self.data_svc.explode('operation', dict(id=self.operation['id'])))[0]
         for member in operation['host_group']:
             for l in await self.planning_svc.select_links(operation, member, phase):
                 await self.agent_svc.perform_action(l)
