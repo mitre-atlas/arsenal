014b7701ea1cf55faa124b03018cd4a92d952fba
david <49954156+privateducky@users.noreply.github.com>
2019-11-30 17:24:05 -0500

    removing api call from stockpile (#245)
    Former-commit-id: 443c5de52703398878197056300fa016138ce28f

--- hook.py
+++ hook.py
@@ -1,5 +1,4 @@
 from plugins.stockpile.app.http import HTTP
-from plugins.stockpile.app.stockpile_api import StockpileApi
 from plugins.stockpile.app.stockpile_svc import StockpileService
 
 name = 'Stockpile'
@@ -8,12 +7,9 @@ address = None
 
 
 async def enable(services):
-    app = services.get('app_svc').application
     file_svc = services.get('file_svc')
     await file_svc.add_special_payload('mission.go', StockpileService(file_svc).dynamically_compile)
 
-    stockpile_api = StockpileApi(services)
     data_svc = services.get('data_svc')
-    app.router.add_route('POST', '/stockpile/ability', stockpile_api.load_ability)
     await data_svc.load_data(directory='plugins/stockpile/data')
     await services.get('contact_svc').register(HTTP(services))
