e967c781d4486fdb0f2a97dfb5877ac0e5aa9c89
Brian Edmonds <bedmonds@mitre.org>
2019-12-11 21:03:32 -0500

    sequential planner stopping condition (#264)
    Former-commit-id: 6ca46c97ffd92f27b8573034229c1664a0a8d3e9

--- app/sequential.py
+++ app/sequential.py
@@ -1,9 +1,12 @@
 class LogicalPlanner:
 
-    def __init__(self, operation, planning_svc):
+    def __init__(self, operation, planning_svc, stopping_conditions=[]):
         self.operation = operation
         self.planning_svc = planning_svc
+        self.stopping_conditions = stopping_conditions
+        self.stopping_condition_met = False
 
     async def execute(self, phase):
-        for link in await self.planning_svc.get_links(operation=self.operation, phase=phase):
+        for link in await self.planning_svc.get_links(operation=self.operation, phase=phase,
+                                                      stopping_conditions=self.stopping_conditions, planner=self):
             await self.operation.apply(link)
