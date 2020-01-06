7c474a5f2f01a2e9a63f0f7670f0c7a7fd5233b0
Brian Edmonds <bedmonds@mitre.org>
2020-01-06 16:14:01 -0500

    Host facts enforced via the paw provenance requirement module (#313)
    * wip
    
    * spacing issue
    
    * fact typo
    
    * adjusting stockpile abilities to include paw_provenance
    
    
    Former-commit-id: 5b678bd76b87b73e35fba55b659c0af73c9643c1

--- app/requirements/paw_provenance.py
+++ app/requirements/paw_provenance.py
@@ -5,15 +5,13 @@ class Requirement(BaseRequirement):
 
     def enforce(self, link, relationships):
         """
-        Given a link and all known fact relationships, check if the link's used fact combination complies
-        with the abilities enforcement mechanism
+        Given a link, check if any of the used facts must comply with paw provenance requirements
         :param link
         :param relationships
         :return: True if it complies, False if it doesn't
         """
         for uf in link.used:
             if self.enforcements.source == uf.trait:
-                for r in self._get_relationships(uf, relationships):
-                    if self.is_valid_relationship([f for f in link.used if f != uf], r):
-                        return True
+                if link.paw == uf.collected_by:
+                    return True
         return False
