868f1e4f5c1b9f71b88b1483f21d36aa09deff37
brianedmonds90 <bedmonds@mitre.org>
2020-01-07 13:31:09 -0500

    simplifying
    Former-commit-id: 31584d4a9b3e349abc01d8f5b1895d9f63f1e59e

--- app/requirements/not_exists.py
+++ app/requirements/not_exists.py
@@ -3,21 +3,6 @@ from plugins.stockpile.app.requirements.base_requirement import BaseRequirement
 
 class Requirement(BaseRequirement):
 
-    # def enforce(self, link, relationships):
-    #     """
-    #     Given a link and relationships ensure that there is NO existence of the enforced relationships
-    #     :param link
-    #     :param relationships
-    #     :return: True if it complies, False if it doesn't
-    #     """
-    #     for uf in link.used:
-    #         if self.enforcements.source == uf.trait:
-    #             for r in relationships:
-    #                 if r.source[0] == self.enforcements.source and self.check_fact(r.source, uf) \
-    #                         and self.check_edge(r.edge):
-    #                     return False
-    #     return True
-
     def enforce(self, link, relationships):
         """
         Given a link and all known fact relationships, check if the link's used fact combination complies
