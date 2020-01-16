5f1050c09cf6ea12893dd49061174595807958f0
David Minter <rminter@mitre.org>
2020-01-16 12:20:24 -0500

    allowing users to ignore specified enforcement modules
    Former-commit-id: 1756a0a138f35ef4c03f74233f12009dacefc792

--- app/sequential.py
+++ app/sequential.py
@@ -1,7 +1,8 @@
 class LogicalPlanner:
 
-    def __init__(self, operation, planning_svc, stopping_conditions=()):
+    def __init__(self, operation, planning_svc, stopping_conditions=(), ignore_enforcement_modules=()):
         self.operation = operation
+        self.operation.ignore_enforcement_modules = ignore_enforcement_modules
         self.planning_svc = planning_svc
         self.stopping_conditions = stopping_conditions
         self.stopping_condition_met = False
