a3704307473653d570c49a034a044d4e588df4aa
Brian Edmonds <bedmonds@mitre.org>
2020-01-16 15:55:57 -0500

    removing http contact as it needs to be in core (#333)
    Former-commit-id: febbcb75274067618ecf02393ae7e6aa13632344

--- hook.py
+++ hook.py
@@ -1,5 +1,4 @@
 from app.objects.c_obfuscator import Obfuscator
-from plugins.stockpile.app.contact.http import HTTP
 from plugins.stockpile.app.stockpile_svc import StockpileService
 from plugins.stockpile.app.contact.gist import GIST
 
@@ -13,7 +12,6 @@ async def enable(services):
     await stockpile_svc.file_svc.add_special_payload('mission.go', stockpile_svc.dynamically_compile)
     await stockpile_svc.data_svc.load_data(directory='plugins/stockpile/data')
     c2_configs = await stockpile_svc.load_c2_config(directory='plugins/stockpile/data/contact')
-    await stockpile_svc.contact_svc.register(HTTP(services, c2_configs['HTTP']))
     await stockpile_svc.contact_svc.register(GIST(services, c2_configs['GIST']))
     await stockpile_svc.data_svc.store(
         Obfuscator(name='plain-text',
