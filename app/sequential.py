7bdf85343358992ab24ef6d21377351b91988991
David Minter <rminter@mitre.org>
2020-01-17 12:06:07 -0500

    Feedback from Brian
    Former-commit-id: 453e8901b1498ed0ef278599e78fab09f625520d

--- app/sequential.py
+++ app/sequential.py
@@ -1,8 +1,7 @@
 class LogicalPlanner:
 
-    def __init__(self, operation, planning_svc, stopping_conditions=(), ignore_enforcement_modules=()):
+    def __init__(self, operation, planning_svc, stopping_conditions=()):
         self.operation = operation
-        self.operation.ignore_enforcement_modules = ignore_enforcement_modules
         self.planning_svc = planning_svc
         self.stopping_conditions = stopping_conditions
         self.stopping_condition_met = False
