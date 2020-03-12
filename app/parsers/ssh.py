96e56ff7b6657b190cff1e3875c271bb42382a07
machan <machan@mitre.org>
2020-03-12 11:59:31 -0400

    ssh parser and fixes for Dump History and Parse SSH Config

---
+++ app/parsers/ssh.py
@@ -0,0 +1,19 @@
+import re
+from app.objects.secondclass.c_relationship import Relationship
+from app.utility.base_parser import BaseParser
+
+
+class Parser(BaseParser):
+
+    def parse(self, blob):
+        relationships = []
+        for ssh_cmd in re.findall(r'[\w.]+@[\w.]+', blob):
+            for mp in self.mappers:
+                source = self.set_value(mp.source, ssh_cmd, self.used_facts)
+                target = self.set_value(mp.target, ssh_cmd, self.used_facts)
+                relationships.append(
+                    Relationship(source=(mp.source, source),
+                                 edge=mp.edge,
+                                 target=(mp.target, target))
+                )
+        return relationships
