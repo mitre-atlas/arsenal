abfbea3b76fb220820204a05fa6beac35d97679c
Caleb Little <50213210+ArtificialErmine@users.noreply.github.com>
2020-04-09 17:23:27 -0400

    virts-1062c (#426)
    * rebase to new head: First pass at phaseless system
    
    * Removing packs

--- app/sequential.py
+++ app/sequential.py
@@ -6,7 +6,7 @@ class LogicalPlanner:
         self.stopping_conditions = stopping_conditions
         self.stopping_condition_met = False
 
-    async def execute(self, phase):
-        for link in await self.planning_svc.get_links(operation=self.operation, phase=phase,
+    async def execute(self, cursor):
+        for link in await self.planning_svc.get_links(operation=self.operation,
                                                       stopping_conditions=self.stopping_conditions, planner=self):
             await self.operation.apply(link)
