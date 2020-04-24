6580bd7d97e9d91441377c9a3b3a6a7467dbf241
Alex <43142871+khyberspache@users.noreply.github.com>
2020-04-24 15:05:36 -0400

    VIRTS-1142/Add donut hook function (#434)
    * intial commit
    
    * updated donut
    
    * adding donut extension hook file
    
    * cleaning up functions
    
    * replace operations
    
    * private
    
    * space

--- app/donut.py
+++ app/donut.py
@@ -1,3 +1,23 @@
-async def donut_handler(args):
-    print('This is where we would configure things to properly handle a .donut file request.')
-    return '', ''
+import donut
+import os
+
+
+async def donut_handler(services, args) -> (str, str):
+    _, file_name = await services.get('file_svc').find_file_path(args.get('file'), location='payloads')
+    dir_path, donut_ext = os.path.split(file_name)
+    exe_path = os.path.join(dir_path, '%s.exe' % donut_ext.split('.')[0])
+    os.replace(src=file_name, dst=exe_path)
+    shellcode = donut.create(file=exe_path)
+    _write_shellcode_to_file(shellcode, exe_path)
+    os.replace(src=exe_path, dst=file_name)
+    return donut_ext, donut_ext  # payload, display_name
+
+""" PRIVATE """
+
+
+def _write_shellcode_to_file(shellcode, file_name):
+    try:
+        with open(file_name, 'wb') as f:
+            f.write(shellcode)
+    except Exception as ex:
+        print(ex)
