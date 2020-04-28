2588b6bc9f83b824603039f659eecb8c44972d67
Michael <mkouremetis16@gmail.com>
2020-04-28 11:01:40 -0400

    Virts 1063 (#431)
    * initial
    
    * planners working with buckets
    
    * remove testing lines
    
    * moving
    
    * moving
    
    * crap wrong one
    
    * cascading changes from core
    
    * working correctly with new planner regime
    
    * sequential logic was wrong
    
    Co-authored-by: elegantmoose <mkouremetis@mitre.org>

--- app/sequential.py
+++ app/sequential.py
@@ -5,8 +5,22 @@ class LogicalPlanner:
         self.planning_svc = planning_svc
         self.stopping_conditions = stopping_conditions
         self.stopping_condition_met = False
+        self.state_machine = ['sequential']
+        self.next_bucket = 'sequential'   # set first (and only) bucket to execute
 
-    async def execute(self, cursor):
-        for link in await self.planning_svc.get_links(operation=self.operation,
-                                                      stopping_conditions=self.stopping_conditions, planner=self):
-            await self.operation.apply(link)
+    async def execute(self):
+        await self.planning_svc.execute_planner(self)
+
+    async def sequential(self):
+        links = await self._get_links()
+        while links:
+            link_ids = [await self.operation.apply(l) for l in links]
+            await self.operation.wait_for_links_completion(link_ids)
+            # new links may be available now (e.g. requirements met)
+            links = await self._get_links()
+        self.next_bucket = None
+
+    async def _get_links(self):
+        return await self.planning_svc.get_links(operation=self.operation,
+                                                 stopping_conditions=self.stopping_conditions,
+                                                 planner=self)
