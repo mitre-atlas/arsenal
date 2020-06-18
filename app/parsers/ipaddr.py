89ef9b18903a0e7d39e451b47df9fc0382a2fc5d
clittle <clittle@mitre.org>
2020-06-18 13:28:07 -0400

    Stashing

---
+++ app/parsers/ipaddr.py
@@ -0,0 +1,26 @@
+import re
+
+from ipaddress import ip_address
+
+from app.objects.secondclass.c_fact import Fact
+from app.objects.secondclass.c_relationship import Relationship
+from app.utility.base_parser import BaseParser
+
+class Parser(BaseParser):
+    def parse(self, blob):
+        IPs = []
+        for ip in re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', blob):
+            if self._is_valid_ip(ip):
+                IPs.append(Relationship(source=Fact(trait='remote.host.ip', value=ip), edge=''))
+        return IPs
+
+    @staticmethod
+    def _is_valid_ip(raw_ip):
+        try:
+            # The following hardcoded addresses are not used to bind to an interface.
+            if raw_ip in ['0.0.0.0', '127.0.0.1']:  # nosec
+                return False
+            ip_address(raw_ip)
+        except BaseException:
+            return False
+        return True
