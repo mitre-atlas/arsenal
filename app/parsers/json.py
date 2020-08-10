9ff86d95d6ece7d77cfebf38aabc0204c756648c
Melanie Chan <61020544+mchan143@users.noreply.github.com>
2020-08-10 08:04:59 -0400

    updated abilities with json custom parser vals and json parser (#478)

--- app/parsers/json.py
+++ app/parsers/json.py
@@ -19,11 +19,11 @@ class Parser(BaseParser):
         json_output = self.load_json(blob)
         if json_output is not None:
             for mp in self.mappers:
-                if 'json_key' not in dir(mp):
+                if 'json_key' not in mp.custom_parser_vals.keys():
                     self.log.warning('JSON Parser not given a json_key, not parsing')
                     continue
-                json_type = mp.json_type if 'json_type' in dir(mp) else None
-                for match in self._get_vals_from_json(json_output, mp.json_key, json_type):
+                json_type = mp.custom_parser_vals['json_type'] if 'json_type' in mp.custom_parser_vals.keys() else None
+                for match in self._get_vals_from_json(json_output, mp.custom_parser_vals['json_key'], json_type):
                     source = self.set_value(mp.source, match, self.used_facts)
                     target = self.set_value(mp.target, match, self.used_facts)
                     relationships.append(
@@ -38,7 +38,7 @@ class Parser(BaseParser):
         Get all values for a specified key recursively from JSON output.
         :param json_output:
         :param key:
-        :param json_type: a list of types to filter matches by. Example options are - 'str', 'int', 'list', 'dict'
+        :param json_type: a type to filter matches by. Example options are - 'str', 'int', 'list', 'dict'
         :return: generator that yields matched values
         """
         if isinstance(json_output, list):
@@ -47,7 +47,7 @@ class Parser(BaseParser):
                     yield res
         elif isinstance(json_output, dict):
             for k, v in json_output.items():
-                if k == key and (json_type is None or v.__class__.__name__ in json_type):
+                if k == key and (json_type is None or v.__class__.__name__ == json_type):
                     yield json.dumps(v) if v.__class__.__name__ in ['list', 'dict'] else v
                 if isinstance(v, list) or isinstance(v, dict):
                     for res in self._get_vals_from_json(v, key, json_type):
