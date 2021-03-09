dfefdcb47129b3e49468124dfd21fa738e88f020
Fred <59877971+mrengstrom@users.noreply.github.com>
2021-03-09 13:15:36 -0600

    fixing the psh command wrapper for b64 no padding obfuscator (#518)

--- app/obfuscators/base64_no_padding.py
+++ app/obfuscators/base64_no_padding.py
@@ -21,8 +21,7 @@ class Obfuscation(BaseObfuscator):
         return 'eval "$(echo %s=== | base64 --decode 2>/dev/null)"' % link.command
 
     def psh(self, link, **kwargs):
-        recoded = b64encode(link.command)
-        return '$string=%s;' +\
+        return '$string="%s";' % link.command +\
             'while($string.Length %4 -ne 0) {$string="$string="};' +\
-            '$ExecutionContext.InvokeCommand.ExpandString(' +\
-            '[System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String($string)))' % recoded
+            'Invoke-Expression $ExecutionContext.InvokeCommand.ExpandString(' +\
+            '[System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String($string)))'
