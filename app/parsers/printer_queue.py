482f9c241f28e9375425966083cfbaf65b043841
New Name <20597974+kaylakraines@users.noreply.github.com>
2021-04-20 16:16:56 -0400

    Add parser to parse result of 'View printer queue' ability

---
+++ app/parsers/printer_queue.py
@@ -0,0 +1,27 @@
+from app.objects.secondclass.c_fact import Fact
+from app.objects.secondclass.c_relationship import Relationship
+from app.utility.base_parser import BaseParser
+import re
+
+class Parser(BaseParser):
+    def parse(self, blob):
+        relationships = []
+        for match in self.line(blob):
+            first_char = match[0]
+            if not first_char.isnumeric():
+                continue
+
+            split_match = re.split(" +", match)
+            file_name = " ".join(split_match[3:-2])
+            file_size = " ".join(split_match[-2:])
+
+            for mp in self.mappers:
+                source = self.set_value(mp.source, file_name, self.used_facts)
+                target = self.set_value(mp.target, file_size, self.used_facts)
+                relationships.append(
+                    Relationship(source=Fact(mp.source, source),
+                                 edge=mp.edge,
+                                 target=Fact(mp.target, target))
+                )
+
+        return relationships
