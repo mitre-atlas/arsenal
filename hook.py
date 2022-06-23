3f322471ecaa90bdaf4f1c412174ed315947cd6d
Marissa R Dotter <mdotter@mitre.org>
2022-06-23 20:00:27 +0000

    WIP: update plugin name

--- hook.py
+++ hook.py
@@ -1,46 +1,46 @@
 from app.objects.c_obfuscator import Obfuscator
 from app.utility.base_world import BaseWorld
-from plugins.stockpile.app.stockpile_svc import StockpileService
+from plugins.arsenal.app.arsenal_svc import ArsenalService
 
-name = 'Stockpile'
-description = 'A stockpile of abilities, adversaries, payloads and planners'
-address = '/plugin/stockpile/gui'
+name = 'Arsenal'
+description = 'An Arsenal-Stockpile hybrid plugin of abilities, adversaries, payloads and planners'
+address = '/plugin/arsenal/gui'
 access = BaseWorld.Access.APP
 
 
 async def enable(services):
-    stockpile_svc = StockpileService(services)
-    services.get('app_svc').application.router.add_route('GET', '/plugin/stockpile/gui', stockpile_svc.splash)
-    await services.get('file_svc').add_special_payload('.donut', 'plugins.stockpile.app.donut.donut_handler')
-    await stockpile_svc.data_svc.store(
+    arsenal_svc = ArsenalService(services)
+    services.get('app_svc').application.router.add_route('GET', '/plugin/arsenal/gui', arsenal_svc.splash)
+    await services.get('file_svc').add_special_payload('.donut', 'plugins.arsenal.app.donut.donut_handler')
+    await arsenal_svc.data_svc.store(
         Obfuscator(name='plain-text',
                    description='Does no obfuscation to any command, instead running it in plain text',
-                   module='plugins.stockpile.app.obfuscators.plain_text')
+                   module='plugins.arsenal.app.obfuscators.plain_text')
     )
-    await stockpile_svc.data_svc.store(
+    await arsenal_svc.data_svc.store(
         Obfuscator(name='base64',
                    description='Obfuscates commands in base64',
-                   module='plugins.stockpile.app.obfuscators.base64_basic')
+                   module='plugins.arsenal.app.obfuscators.base64_basic')
     )
-    await stockpile_svc.data_svc.store(
+    await arsenal_svc.data_svc.store(
         Obfuscator(name='base64jumble',
                    description='Obfuscates commands in base64, then adds characters to evade base64 detection. '
                                'Disclaimer: this may cause duplicate links to run.',
-                   module='plugins.stockpile.app.obfuscators.base64_jumble')
+                   module='plugins.arsenal.app.obfuscators.base64_jumble')
     )
-    await stockpile_svc.data_svc.store(
+    await arsenal_svc.data_svc.store(
         Obfuscator(name='caesar cipher',
                    description='Obfuscates commands through a caesar cipher algorithm, which uses a randomly selected '
                                'shift value.',
-                   module='plugins.stockpile.app.obfuscators.caesar_cipher')
+                   module='plugins.arsenal.app.obfuscators.caesar_cipher')
     )
-    await stockpile_svc.data_svc.store(
+    await arsenal_svc.data_svc.store(
         Obfuscator(name='base64noPadding',
                    description='Obfuscates commands in base64, then removes padding',
-                   module='plugins.stockpile.app.obfuscators.base64_no_padding')
+                   module='plugins.arsenal.app.obfuscators.base64_no_padding')
     )
-    await stockpile_svc.data_svc.store(
+    await arsenal_svc.data_svc.store(
         Obfuscator(name='steganography',
                    description='Obfuscates commands through image-based steganography',
-                   module='plugins.stockpile.app.obfuscators.steganography')
+                   module='plugins.arsenal.app.obfuscators.steganography')
     )
