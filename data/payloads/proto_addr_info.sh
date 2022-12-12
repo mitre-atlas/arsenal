b34ec5ee58dc509b574e870e457be58cb9e974fd
Aidan Fennelly <afennelly@mitre.org>
2022-12-12 05:47:25 +0000

    list primary addr, connectd to internet Ref #23

--- data/payloads/proto_addr_info.sh
+++ data/payloads/proto_addr_info.sh
@@ -3,5 +3,5 @@
 # check if "ip" command is available
 if command -v ip &> /dev/null; then 
     # look at protocol addresses of all available devices
-    ip -4 addr show | grep inet | awk '{print $NF " " $2}'
+    ip -4 addr show primary | grep -A 2 "LOWER_UP" | grep inet | awk '{print $NF " " $2}'
 fi
