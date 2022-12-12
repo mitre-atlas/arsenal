7302c512705b54808f6813d0ca6a05e08f1a7657
Aidan Fennelly <afennelly@mitre.org>
2022-12-12 06:23:35 +0000

    enable script arg to specify dev name. Ref #23

--- data/payloads/proto_addr_info.sh
+++ data/payloads/proto_addr_info.sh
@@ -3,5 +3,10 @@
 # check if "ip" command is available
 if command -v ip &> /dev/null; then 
     # look at protocol addresses of all available devices
-    ip -4 addr show primary | grep -A 2 "LOWER_UP" | grep inet | awk '{print $NF " " $2}'
+    if [ $# -eq 0 ]; then
+        ip -4 addr show | grep -A 2 "LOWER_UP" | grep inet | awk '{print $NF " " $2}'
+    # look at protocol address of a specific device
+    else
+        ip -4 addr show dev $1 | grep -A 2 "LOWER_UP" | grep inet | awk '{print $NF " " $2}'
+    fi
 fi
