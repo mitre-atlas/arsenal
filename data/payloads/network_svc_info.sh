fc3bf99d1bed1907fa28f75775a0a4b4fa6f3a40
Aidan Fennelly <afennelly@mitre.org>
2023-01-10 23:53:44 +0000

    omit nmap output from being captured by parser

--- data/payloads/network_svc_info.sh
+++ data/payloads/network_svc_info.sh
@@ -14,7 +14,7 @@ TRG=$1
 if command -v nmap &> /dev/null; then
     # perform ping scan of network and capture "up" hosts in UP_HOSTS_LIST
     UP_HOSTS_LIST=$(nmap -sn $HOST_DISC_OPTS $TIME_OPTS $TRG | awk '/Nmap scan/{gsub(/[()]/,"",$NF); print $NF}')
-    nmap -oG up_hosts_scan.txt $UP_HOSTS_LIST
+    nmap -oG up_hosts_scan.txt $UP_HOSTS_LIST &> /dev/null
     cat up_hosts_scan.txt | grep -v "Status" | grep -v "Nmap"
     rm up_hosts_scan.txt
 fi
