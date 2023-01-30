1b281299a8d54de2e854efe972b25d3b26a685de
Aidan Fennelly <afennelly@mitre.org>
2023-01-30 17:43:42 +0000

    echo binding addrs instead of entry point paths

--- payloads/discover_torchserve_api.sh
+++ payloads/discover_torchserve_api.sh
@@ -10,14 +10,12 @@ if command -v curl &> /dev/null; then
     do
         ping_path="$bind_addr/ping"
         description_path="$bind_addr/api-description"
-        inference_entry_point_path="$bind_addr/predictions"
-        models_entry_point_path="$bind_addr/models"
         # first, try "Health API" (https://pytorch.org/serve/inference_api.html#health-check-api),
         # Management API will not return "Healthy" in the output
         if curl -s $ping_path | grep "Healthy" &> /dev/null; then
-            echo "INFERENCE_API $inference_entry_point_path"
+            echo "INFERENCE_API $bind_addr"
         elif curl -s $description_path | grep "List registered models in TorchServe." &> /dev/null; then
-            echo "MANAGEMENT_API $models_entry_point_path"
+            echo "MANAGEMENT_API $bind_addr"
         fi
     done
 fi
