bb716eba3e24038b9adcb97ba2e0f9fd98bf8bbe
Marissa R Dotter <mdotter@mitre.org>
2023-02-02 03:59:07 +0000

    updates script to return ip address and model name to parse

--- payloads/discover_prediction_endpoint.sh
+++ payloads/discover_prediction_endpoint.sh
@@ -5,8 +5,8 @@ if command -v curl &> /dev/null; then
     # this is only tested for one model in ip_addr/models
     if curl -s $model | grep "modelName" >> /dev/null; then
         model_name="$(curl -s $model | grep "modelName" | awk -F '"' '{print $4}')"
-        echo "MODEL $model_name"
+        echo "$1 $model_name"
     else
-        echo "MODEL null"
+        echo "$1 null"
     fi
 fi
