a8429f3a6a7bba43ba6c392d116d0cc1e949220c
Marissa R Dotter <mdotter@mitre.org>
2023-02-21 05:44:14 +0000

    WIP: collects api endpoints for model and prediction api

--- app/parsers/endpoint.py
+++ app/parsers/endpoint.py
@@ -2,29 +2,44 @@ from app.objects.secondclass.c_fact import Fact
 from app.objects.secondclass.c_relationship import Relationship
 from app.utility.base_parser import BaseParser
 
+import logging
 
 class Parser(BaseParser):
     """
     Parser that will connect the binding address to the prediction endpoint.
 
     """
-
+    def __init__(self, parser_info):
+        self.logs = logging.getLogger('ENDPOINT')
+    
     def parse(self, blob):
         relationships = []
-        # create different fact mappings, dependent on which TS service is discovered
-        # NOTE: when TorchServe (TS) starts, it starts two web services:
-        #   - Inference API 
-        #   - Management API
+        # retrieve collected endpoint addresses
+        endpoint_facts = []
+        for used_fact in self.used_facts:
+            log_msg = "fact name: {}".format(used_fact.name)
+            self.logs.info(log_msg)
+            log_msg = "fact value: {}".format(used_fact.value)
+            self.logs.info(log_msg)
+            
+            if 'address' in used_fact.name:
+                endpoint_facts.append(used_fact.value)
+   
+        if len(endpoint_facts) > 1:
+            raise NotImplementedError
+        else:
+            inf_bind_addr = endpoint_facts[0]
+        
         for line in self.line(blob):
             # parser expects api endpoint, <model-name> (ex. 10.X.X.X resnet-18, fasterrcnn)
-            bind_addr, model_name = line.split(' ')
+            _, model_name = line.split(' ')
             for mp in self.mappers:
                 # only creation of target.model_server.framework fact is supported ("source" fact of "Relationship")
                 if 'model_server.framework' not in mp.source:
                     raise NotImplementedError
                 # use BaseParse.used_facts to get associated inference_api
                 if model_name != 'null':
-                    pred_endpoint = bind_addr + '/predictions/' + model_name
+                    pred_endpoint = inf_bind_addr + '/predictions/' + model_name
                     relationships.append(
                         Relationship(source=Fact(mp.source, 'TorchServe'),
                                         edge=mp.edge,
