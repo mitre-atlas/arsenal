95d41bc5e3d6d089f791054d000feee3fca1ddb0
Marissa R Dotter <mdotter@mitre.org>
2023-03-29 01:24:33 +0000

    splits functionality into separate methods

--- app/parsers/torchserve_api.py
+++ app/parsers/torchserve_api.py
@@ -7,31 +7,42 @@ class Parser(BaseParser):
     """
     Parser that will extract relevant information about an API that has been
         scanned.
+    
+    Parser expects <api_type> <endpoint_bind_addr> 
+        (ex. INFERENCE_API 10.X.Y.Y:<port>)
         
-    Parse Fact store for #{target.api.binding_address} and link facts with found
-        API endpoints that contain what is being searched for.
-
+    Parse Fact store for #{target.model_server} and link facts
+        with found API endpoints. When TorchServe (TS) starts
+        it starts two web services:
+           - Inference API 
+           - Management API
     """
 
     def parse(self, blob):
         relationships = []
-        # create different fact mappings, dependent on which TS service is discovered
-        # NOTE: when TorchServe (TS) starts, it starts two web services:
-        #   - Inference API 
-        #   - Management API
         for line in self.line(blob):
-            # parser expects <api_type> <endpoint_bind_addr> (ex. INFERENCE_API 10.X.Y.Y:8080)
             api_type, bind_addr = line.split()
             for mp in self.mappers:
-                # only creation of target.model_server.framework fact is supported ("source" fact of "Relationship")
+                # only creation of target.model_server.framework fact is supported
                 if 'model_server.framework' not in mp.source:
                     raise NotImplementedError
-                # ensure relationship creation is correct; dependent on the <api_type>
-                mp_trg_type = mp.target.split('.').pop().split('_')[0]
-                if mp_trg_type in api_type.lower():
+                target_type = self._map_target_type(mp)
+                if target_type in api_type.lower():
                     relationships.append(
                         Relationship(source=Fact(mp.source, 'TorchServe'),
                                      edge=mp.edge,
                                      target=Fact(mp.target, bind_addr))
                     )
         return relationships
+
+    def _map_target_type(self, map):
+        """
+        Funtionality to extract api endpoint type from target
+        """
+        
+        targets = map.target.split('.').pop()
+        target_type = targets.split('_')[0]
+        
+        return target_type
+        
+        
