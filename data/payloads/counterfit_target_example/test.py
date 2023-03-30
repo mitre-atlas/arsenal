e75a7f5da46bca5a2155b5887adf2cb2738519fa
Marissa R Dotter <mdotter@mitre.org>
2023-03-30 21:14:09 +0000

    removes unused functionality

--- data/payloads/counterfit_target_example/test.py
+++
@@ -1,9 +0,0 @@
-from torchserve import TorchServeImageNetClassifier
-from skimage.io import imread
-
-target = TorchServeImageNetClassifier()
-target.load()
-
-im=imread("3dogs.jpg")[None,...]
-scores = target.predict(im)
-print(scores)
