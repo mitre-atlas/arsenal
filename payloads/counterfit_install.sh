b253c906615520d45e19043241322ee1e315991f
Marissa R Dotter <mdotter@mitre.org>
2023-06-06 20:54:35 +0000

    when installing cf as counterfit[dev] it installs as counterfit vs CounterFit

--- payloads/counterfit_install.sh
+++ payloads/counterfit_install.sh
@@ -6,7 +6,7 @@ if [ ! -d "$HOME/venv/cf_venv" ]; then
 	fi
 	python3 -m venv $HOME/venv/cf_venv
 fi
-if ! { $HOME/venv/cf_venv/bin/pip list CounterFit | grep 'CounterFit'; } >/dev/null 2>&1; then
+if ! { $HOME/venv/cf_venv/bin/pip list counterfit | grep 'counterfit'; } >/dev/null 2>&1; then
 	$HOME/venv/cf_venv/bin/python3 -m pip install -U pip wheel setuptools >/dev/null
 	$HOME/venv/cf_venv/bin/python3 -m pip install git+https://github.com/Azure/counterfit.git@main >/dev/null
 	$HOME/venv/cf_venv/bin/python3 -m pip install counterfit[dev] >/dev/null
